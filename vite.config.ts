871626d8a987de28f4b99ae4bd75559fbaca1cba
daozealot <liubomir.yordanov@gmail.com>
2024-12-01 16:33:48 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./Diceable-views",
+  base: "/Diceable-views",
   plugins: [react()],
 })
